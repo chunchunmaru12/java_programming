bcfa6b2e72ff26a8e62374ac4354297d040d9500
chunchunmaru12 <princekhan456.pk10@gmail.com>
2025-01-28 10:46:37 +0545

    Mouse event Listeners and Random Number Challenge using Mouse events

--- Swing Component/frames/ProfileComponent.java
+++ Swing Component/frames/ProfileComponent.java
@@ -4,6 +4,5 @@ public class ProfileComponent extends JFrame{
     public ProfileComponent(){
         setTitle("Profile");
         setSize(400, 400);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
